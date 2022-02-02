889b548a871128f3c3c63882706dcf9b219fb16c
Laurent Mathieu <mathieu.laurent1995@gmail.com>
2022-02-02 11:12:24 +0100

    secu

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -5,16 +5,21 @@ namespace App\Form;
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('media')
-            ->add('category')
+            ->add('name', TextType::class)
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'multiple' => false
+            ])
         ;
     }
 
