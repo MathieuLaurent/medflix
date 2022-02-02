ca8108f665243d29b1082cb252c275bff1df0c3e
AntoninHAROUARD <94467792+AntoninHAROUARD@users.noreply.github.com>
2022-02-02 15:43:21 +0100

    commence partie user

--- src/Controller/MediaController.php
+++ src/Controller/MediaController.php
@@ -10,17 +10,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class MediaController extends AbstractController
 {
     
-
-// /!\ PAS FINI
     #[Route("/files", name:"files")]
     public function files(MediaRepository $media) : Response
     {    
 
+        // Form preremplit 
+
+
         return $this->render('pages/files.html.twig', [
-            //findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
-            //$service = $repository->findBy(array('name' => 'Registration'),array('name' => 'ASC'),1 ,0)[0];
             'media' =>$media->findBy([], array('name' => 'ASC'), 20, 0),
-            "test" => "karibou"
         ]);
 
     }
