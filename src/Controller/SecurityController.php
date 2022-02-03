85a597d1944cf9133bda302123b12d8b83406c2c
Laetitia Ashry <ashry.laetitia@gmail.com>
2022-02-03 10:40:14 +0100

    accueil / login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,20 +10,20 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+            return $this->redirectToRoute('files');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('pages/accueil.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     /**
