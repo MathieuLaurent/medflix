3f88f64bbcbc6a2a74325bd1fadadf6b4fff3ac6
Lee <95095368+JulienB63100@users.noreply.github.com>
2022-02-04 16:55:59 +0100

    modif

--- src/Controller/MediaController.php
+++ src/Controller/MediaController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\MediaRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,11 +13,12 @@ class MediaController extends AbstractController
 {
     
     #[Route("/files", name:"files")]
-    public function files(MediaRepository $media) : Response
+    public function files(MediaRepository $media, CategoryRepository $category) : Response
     {    
 
         return $this->render('pages/files.html.twig', [
             'media' =>$media->findBy([], array('name' => 'ASC'), 20, 0),
+            'categorys'=> $category-> findByParentField()
         ]);
 
     }
