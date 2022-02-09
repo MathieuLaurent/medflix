4934e2c356758557cc03fe1da99b74f6cd97c1d9
Laurent Mathieu <mathieu.laurent1995@gmail.com>
2022-02-09 09:30:01 +0100

    debug edition media

--- src/Controller/EditorMediaController.php
+++ src/Controller/EditorMediaController.php
@@ -118,12 +118,19 @@ class EditorMediaController extends AbstractController
             
             $edit = $mediaRepository->find($id);
             $lien = $edit-> getLink();
+            $extension = $edit->getExtension();
             if(!empty($lien)){
-            unlink($this->getParameter('kernel.project_dir').'/public/img/'.$lien);
-            unlink($this->getParameter('kernel.project_dir').'/public/imgMiniature/'.$lien);
-            unlink($this->getParameter('kernel.project_dir').'/public/imgInter/'.$lien);
-            unlink($this->getParameter('kernel.project_dir').'/public/pdf/'.$lien);
-            unlink($this->getParameter('kernel.project_dir').'/public/video/'.$lien);
+                if($extension == "jpg" || $extension == "png" || $extension =="jpeg" || $extension == "gif"){
+                    unlink($this->getParameter('kernel.project_dir').'/public/img/'.$lien);
+                    unlink($this->getParameter('kernel.project_dir').'/public/imgMiniature/'.$lien);
+                    unlink($this->getParameter('kernel.project_dir').'/public/imgInter/'.$lien);
+                }
+                elseif($extension == "pdf"){
+                    unlink($this->getParameter('kernel.project_dir').'/public/pdf/'.$lien);
+                }
+                elseif($extension == "mp4" || $extension == "webm" || $extension == "avi"){
+                    unlink($this->getParameter('kernel.project_dir').'/public/video/'.$lien);
+                }
             
             $medium->setLink(''); 
             $medium->setExtension('');
